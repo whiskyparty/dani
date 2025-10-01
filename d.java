d4dc5ed1e6346bc33a5260ad91a0fb23bfc82b54
whiskyparty <danielacristinabravobravo@gmail.com>
2025-09-30 21:16:11 -0400

    Add files via upload

---
+++ d.java
@@ -0,0 +1,29 @@
+conts nombreusuario = prompt("ingrese el nombre de usuario");
+if (nombreusuario) {
+    console.log("hola,${nombreusuario} bienvenido")
+    else {
+        console.log("hola invitado, bienvenido")
+    }
+}
+
+let numero1 =prompt("ingrese el primer numero")
+let numero2 =prompt("ingrese el segundo numero")
+if (numero1>numero2){
+    alert("una de las entradas no es un numero valido")
+    }else{
+    //estructura if-else
+    if (numero1>numero2){}
+        alert("el primer numero (+numero1+)"es el mayor")
+    }else if (numero2>numero1){
+        alert("el segundo numero (+numero2+)"es el mayor")
+    }else{
+        alert("los numeros son iguales")
+        
+    }
+
+    
+ 
+
+
+
+
